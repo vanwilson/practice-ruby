3fc83375e3927c16c6cd9029bbd72e319bb4fbf6
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-17 12:30:04 -0500

    Create store_item.rb

---
+++ store_item.rb
@@ -0,0 +1,19 @@
+item1 = {
+  :name => "Coca-Cola",
+  :size => "16 oz.",
+  :price => 2.50,
+}
+
+item2 = {
+  name: "Coca-Cola Zero",
+  size: "8 oz.",
+  price: 1.19,
+}
+
+item3 = {
+  :name => "Cherry Vanilla Coca-Cola",
+  :size => "1 Liter",
+  :price => 5.99,
+}
+
+puts "#{item1[:size]} of #{item1[:name]} is $#{item1[:price]}"
