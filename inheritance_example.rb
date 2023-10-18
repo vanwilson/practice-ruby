218b1240ddcdec36c2b90d87eae8b25da503f914
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-18 12:29:33 -0500

    add Vehicle class for bike and car to inherit

---
+++ inheritance_example.rb
@@ -0,0 +1,75 @@
+# attempt on my own
+
+# class Car(input_options)
+#   attr_reader speed: direction:
+#   def initialize
+#     @speed = 0
+#     @direction = "north"
+#   end
+
+#   def brake
+#     @speed = 0
+#   end
+
+#   def accelerate
+#     @speed += 10
+#   end
+
+#   def turn(new_direction)
+#     @direction = new_direction
+#   end
+
+#   def honk_horn
+#     puts "Beeeeeeep!"
+#   end
+# end
+
+# class Bike < Car
+#   def ring_bell
+#     puts "Ring ring!"
+#   end
+# end
+
+# car = Car.new
+# bike = Bike.new
+# p car.honk_horn
+# p bike.ring_bell
+# p car.accelerate
+# p bike.accelerate * 2
+
+# from example taught from Amanda
+class Vehicle
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car = Car.new
+car.honk_horn
+bike = Bike.new
+bike.ring_bell
